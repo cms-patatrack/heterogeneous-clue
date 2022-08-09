423a15abddda99c2878d3aa08cb840385061d28a
Juan Jose Olivera Loyola <jolivera@patatrack02.cern.ch>
2022-08-09 15:10:35 +0200

    Added Support for nvidia gpus in patatrack02

--- src/sycl/plugin-PointsLoaderValidator/ValidatorExecutor.cc
+++ src/sycl/plugin-PointsLoaderValidator/ValidatorExecutor.cc
@@ -21,7 +21,7 @@ public:
 private:
   void produce(edm::Event& event, edm::EventSetup const& eventSetup) override;
   template<class T>
-  bool arraysAreEqual(T* devicePtr, std::vector<T> trueDataArr, int n);
+  bool arraysAreEqual(T* devicePtr, std::vector<T> trueDataArr, int n, sycl::queue stream);
   edm::EDGetTokenT<cms::sycltools::Product<PointsCloudSYCL>> tokenPC_;
 };
 
@@ -29,14 +29,20 @@ ValidatorPointsCloudToSYCL::ValidatorPointsCloudToSYCL(edm::ProductRegistry& reg
     : tokenPC_(reg.consumes<cms::sycltools::Product<PointsCloudSYCL>>()) {}
 
 template<class T>
-bool ValidatorPointsCloudToSYCL::arraysAreEqual(T* devicePtr, std::vector<T> trueDataArr, int n) {
+bool ValidatorPointsCloudToSYCL::arraysAreEqual(T* devicePtr, std::vector<T> trueDataArr, int n, sycl::queue stream) {
   bool sameValue = true;
-    for(int i = 0; i < n; i++) {
-      if (devicePtr[i] != trueDataArr[i]) {
-        sameValue = false;
-        break;
-      }
+
+  T* host = new T[n];
+  stream.memcpy(host, devicePtr, sizeof(T)*n);
+
+  for(int i = 0; i < n; i++) {
+    if (host[i] != trueDataArr[i]) {
+      sameValue = false;
+      break;
     }
+  }
+
+  delete[] host;
 
     return sameValue;
 }
@@ -53,34 +59,34 @@ void ValidatorPointsCloudToSYCL::produce(edm::Event& event, edm::EventSetup cons
   assert(pcDevice.n == pcTrueData->n);
   
   std::cout << "Are device and trueData x's equal?\n";
-  assert(arraysAreEqual(pcDevice.x.get(), pcTrueData->x, pcDevice.n));
+  assert(arraysAreEqual(pcDevice.x.get(), pcTrueData->x, pcDevice.n, ctx.stream()));
 
   std::cout << "Are device and trueData y's equal?\n";
-  assert(arraysAreEqual(pcDevice.y.get(), pcTrueData->y, pcDevice.n));
+  assert(arraysAreEqual(pcDevice.y.get(), pcTrueData->y, pcDevice.n, ctx.stream()));
 
   std::cout << "Are device and trueData layer's equal?\n";
-  assert(arraysAreEqual(pcDevice.layer.get(), pcTrueData->layer, pcDevice.n));
+  assert(arraysAreEqual(pcDevice.layer.get(), pcTrueData->layer, pcDevice.n, ctx.stream()));
 
   std::cout << "Are device and trueData weight's equal?\n";
-  assert(arraysAreEqual(pcDevice.weight.get(), pcTrueData->weight, pcDevice.n));
+  assert(arraysAreEqual(pcDevice.weight.get(), pcTrueData->weight, pcDevice.n, ctx.stream()));
 
   std::vector<float> fZeros(pcDevice.n, 0);
   std::vector<int> iZeros(pcDevice.n, 0);
 
   std::cout << "Are device  rho initially 0?\n";
-  assert(arraysAreEqual(pcDevice.rho.get(), fZeros, pcDevice.n));
+  assert(arraysAreEqual(pcDevice.rho.get(), fZeros, pcDevice.n, ctx.stream()));
 
   std::cout << "Are device  delta initially 0?\n";
-  assert(arraysAreEqual(pcDevice.delta.get(), fZeros, pcDevice.n));
+  assert(arraysAreEqual(pcDevice.delta.get(), fZeros, pcDevice.n, ctx.stream()));
 
   std::cout << "Are device nearestHigher initially 0?\n";
-  assert(arraysAreEqual(pcDevice.nearestHigher.get(), iZeros, pcDevice.n));
+  assert(arraysAreEqual(pcDevice.nearestHigher.get(), iZeros, pcDevice.n, ctx.stream()));
 
   std::cout << "Are device clusterIndex initially 0?\n";
-  assert(arraysAreEqual(pcDevice.clusterIndex.get(), iZeros, pcDevice.n));
+  assert(arraysAreEqual(pcDevice.clusterIndex.get(), iZeros, pcDevice.n, ctx.stream()));
 
   std::cout << "Are device clusterIndex initially 0?\n";
-  assert(arraysAreEqual(pcDevice.isSeed.get(), iZeros, pcDevice.n));
+  assert(arraysAreEqual(pcDevice.isSeed.get(), iZeros, pcDevice.n, ctx.stream()));
 
   // std::cout << "Printing x's\n";
   // for (int i = 0; i < 10; i++) {
