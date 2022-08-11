b7d53a16e6720a973bb824ab47e18f299c17c50e
Parsifal-2045 <ferragina.luca@gmail.com>
2022-08-11 16:31:40 +0200

    [sycl] updated producer name

--- src/sycl/bin/main.cc
+++ src/sycl/bin/main.cc
@@ -104,8 +104,8 @@ int main(int argc, char** argv) try {
   std::vector<std::string> edmodules;
   std::vector<std::string> esmodules;
   if (not empty) {
-    edmodules = { "PointsCloudToSYCL", "CLUESYCLClusterizer"};
-    esmodules = { "PointsCloudESProducer" };
+    edmodules = {"PointsCloudProducer", "CLUESYCLClusterizer"};
+    esmodules = {"PointsCloudESProducer"};
     if (transfer) {
       // add modules for transfer
     }
