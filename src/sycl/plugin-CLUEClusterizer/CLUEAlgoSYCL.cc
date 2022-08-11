9e5b3e48809429d894fb94c7a8c29eba4fd86bbd
Parsifal-2045 <ferragina.luca@gmail.com>
2022-08-11 16:39:27 +0200

    [sycl] implemented copying and clustering both operation now happen a single plugin

--- src/sycl/plugin-CLUEClusterizer/CLUEAlgoSYCL.cc
+++ src/sycl/plugin-CLUEClusterizer/CLUEAlgoSYCL.cc
@@ -1,64 +1,110 @@
 #include <CL/sycl.hpp>
+
+#include "DataFormats/PointsCloud.h"
+
 #include "CLUEAlgoSYCL.h"
 #include "CLUEAlgoKernels.h"
 
-CLUEAlgoSYCL::CLUEAlgoSYCL(float dc, float rhoc, float outlierDeltaFactor, sycl::queue stream)
-    : dc_{dc}, rhoc_{rhoc}, outlierDeltaFactor_{outlierDeltaFactor}, q_{stream} {
-  unsigned int reserve = 1000000;
-  //allocate memory
-  d_hist = cms::sycltools::make_device_shared_uninitialized<LayerTilesSYCL[]>(NLAYERS, stream);
-  d_seeds = cms::sycltools::make_device_shared_uninitialized<sycltools::VecArray<int, maxNSeeds>>(stream);
-  d_followers =
-      cms::sycltools::make_device_shared_uninitialized<sycltools::VecArray<int, maxNFollowers>[]>(reserve, stream);
-  //clear set
-  stream.memset(d_hist.get(), 0x00, sizeof(LayerTilesSYCL) * NLAYERS);
-  stream.memset(d_seeds.get(), 0x00, sizeof(sycltools::VecArray<int, maxNSeeds>));
-  stream.memset(d_followers.get(), 0x00, sizeof(sycltools::VecArray<int, maxNFollowers>) * reserve).wait();
+CLUEAlgoSYCL::CLUEAlgoSYCL(float const &dc,
+                           float const &rhoc,
+                           float const &outlierDeltaFactor,
+                           sycl::queue const &stream,
+                           int const &numberOfPoints)
+    : d_points{stream, numberOfPoints}, dc_{dc}, rhoc_{rhoc}, outlierDeltaFactor_{outlierDeltaFactor}, queue_{stream} {
+  init_device();
 }
 
-using view = PointsCloudSYCL::PointsCloudSYCLView;
+CLUEAlgoSYCL::~CLUEAlgoSYCL() {
+  if (queue_) {
+    free_device();
+  }
+}
 
-void CLUEAlgoSYCL::makeClusters(PointsCloudSYCL const& pc) {
+void CLUEAlgoSYCL::init_device() {
+  unsigned int reserve = 1000000;
+  d_hist = sycl::malloc_device<LayerTilesSYCL>(NLAYERS, *queue_);
+  d_seeds = sycl::malloc_device<cms::sycltools::VecArray<int, maxNSeeds>>(1, *queue_);
+  d_followers = sycl::malloc_device<cms::sycltools::VecArray<int, maxNFollowers>>(reserve, *queue_);
+}
+
+void CLUEAlgoSYCL::free_device() {
+  sycl::free(d_hist, *queue_);
+  sycl::free(d_seeds, *queue_);
+  sycl::free(d_followers, *queue_);
+}
+
+void CLUEAlgoSYCL::setup(PointsCloud const &host_pc) {
+  // copy input variables
+  (*queue_).memcpy(d_points.x.get(), host_pc.x.data(), sizeof(float) * host_pc.n);
+  (*queue_).memcpy(d_points.y.get(), host_pc.y.data(), sizeof(float) * host_pc.n);
+  (*queue_).memcpy(d_points.layer.get(), host_pc.layer.data(), sizeof(int) * host_pc.n);
+  (*queue_).memcpy(d_points.weight.get(), host_pc.weight.data(), sizeof(float) * host_pc.n);
+  // initialize result and internal variables
+  (*queue_).memset(d_points.rho.get(), 0x00, sizeof(float) * host_pc.n);
+  (*queue_).memset(d_points.delta.get(), 0x00, sizeof(float) * host_pc.n);
+  (*queue_).memset(d_points.nearestHigher.get(), 0x00, sizeof(int) * host_pc.n);
+  (*queue_).memset(d_points.clusterIndex.get(), 0x00, sizeof(int) * host_pc.n);
+  (*queue_).memset(d_points.isSeed.get(), 0x00, sizeof(int) * host_pc.n);
+  (*queue_).memset(d_hist, 0x00, sizeof(LayerTilesSYCL) * NLAYERS);
+  (*queue_).memset(d_seeds, 0x00, sizeof(cms::sycltools::VecArray<int, maxNSeeds>));
+  (*queue_).memset(d_followers, 0x00, sizeof(cms::sycltools::VecArray<int, maxNFollowers>) * host_pc.n).wait();
+  d_points.n = host_pc.n;
+}
+
+void CLUEAlgoSYCL::makeClusters(PointsCloud const &host_pc) {
+  setup(host_pc);
   // calculate rho, delta and find seeds
   // 1 point per thread
-  auto d_points = pc.view();
   const int numThreadsPerBlock = 256;  // ThreadsPerBlock = work-group size
   const sycl::range<3> blockSize(numThreadsPerBlock, 1, 1);
-  const sycl::range<3> gridSize(ceil(pc.n / static_cast<float>(blockSize[0])), 1, 1);
+  const sycl::range<3> gridSize(ceil(d_points.n / static_cast<float>(blockSize[0])), 1, 1);
+  PointsCloudSYCL::PointsCloudSYCLView *d_points_view = d_points.view();
 
-  q_.submit([&](sycl::handler &cgh) {
+  (*queue_).submit([&](sycl::handler &cgh) {
     //SYCL kernels cannot capture by reference - need to reassign pointers inside the submit to pass by value
-    auto d_hist_kernel = d_hist.get();
-    cgh.parallel_for(sycl::nd_range<3>(gridSize * blockSize, blockSize),
-                     [=](sycl::nd_item<3> item) { kernel_compute_histogram(d_hist_kernel, d_points, item); });
-  });
-
-  q_.submit([&](sycl::handler &cgh) {
-    auto d_hist_kernel = d_hist.get();
-    auto dc_kernel = dc_;
+    auto d_hist_kernel = d_hist;
+    auto num_points_kernel = d_points.n;
     cgh.parallel_for(sycl::nd_range<3>(gridSize * blockSize, blockSize), [=](sycl::nd_item<3> item) {
-      kernel_calculate_density(d_hist_kernel, d_points, dc_kernel, item);
+      kernel_compute_histogram(d_hist_kernel, d_points_view, num_points_kernel, item);
     });
   });
 
-  q_.submit([&](sycl::handler &cgh) {
-    auto d_hist_kernel = d_hist.get();
+  (*queue_).submit([&](sycl::handler &cgh) {
+    auto d_hist_kernel = d_hist;
+    auto dc_kernel = dc_;
+    auto num_points_kernel = d_points.n;
+    cgh.parallel_for(sycl::nd_range<3>(gridSize * blockSize, blockSize), [=](sycl::nd_item<3> item) {
+      kernel_calculate_density(d_hist_kernel, d_points_view, dc_kernel, num_points_kernel, item);
+    });
+  });
+
+  (*queue_).submit([&](sycl::handler &cgh) {
+    auto d_hist_kernel = d_hist;
     auto outlierDeltaFactor_kernel = outlierDeltaFactor_;
     auto dc_kernel = dc_;
+    auto num_points_kernel = d_points.n;
     cgh.parallel_for(sycl::nd_range<3>(gridSize * blockSize, blockSize), [=](sycl::nd_item<3> item) {
-      kernel_calculate_distanceToHigher(d_hist_kernel, d_points, outlierDeltaFactor_kernel, dc_kernel, item);
+      kernel_calculate_distanceToHigher(
+          d_hist_kernel, d_points_view, outlierDeltaFactor_kernel, dc_kernel, num_points_kernel, item);
     });
   });
 
-  q_.submit([&](sycl::handler &cgh) {
-    auto d_seeds_kernel = d_seeds.get();
-    auto d_followers_kernel = d_followers.get();
+  (*queue_).submit([&](sycl::handler &cgh) {
+    auto d_seeds_kernel = d_seeds;
+    auto d_followers_kernel = d_followers;
     auto outlierDeltaFactor_kernel = outlierDeltaFactor_;
     auto dc_kernel = dc_;
     auto rhoc_kernel = rhoc_;
+    auto num_points_kernel = d_points.n;
     cgh.parallel_for(sycl::nd_range<3>(gridSize * blockSize, blockSize), [=](sycl::nd_item<3> item) {
-      kernel_find_clusters(
-          d_seeds_kernel, d_followers_kernel, d_points, outlierDeltaFactor_kernel, dc_kernel, rhoc_kernel, item);
+      kernel_find_clusters(d_seeds_kernel,
+                           d_followers_kernel,
+                           d_points_view,
+                           outlierDeltaFactor_kernel,
+                           dc_kernel,
+                           rhoc_kernel,
+                           num_points_kernel,
+                           item);
     });
   });
 
@@ -66,12 +112,12 @@ void CLUEAlgoSYCL::makeClusters(PointsCloudSYCL const& pc) {
   // 1 point per seeds
   const sycl::range<3> gridSize_nseeds(ceil(maxNSeeds / static_cast<double>(blockSize[0])), 1, 1);
 
-  q_.submit([&](sycl::handler &cgh) {
-    auto d_seeds_kernel = d_seeds.get();
-    auto d_followers_kernel = d_followers.get();
+  (*queue_).submit([&](sycl::handler &cgh) {
+    auto d_seeds_kernel = d_seeds;
+    auto d_followers_kernel = d_followers;
     cgh.parallel_for(sycl::nd_range<3>(gridSize_nseeds * blockSize, blockSize), [=](sycl::nd_item<3> item) {
-      kernel_assign_clusters(d_seeds_kernel, d_followers_kernel, d_points, item);
+      kernel_assign_clusters(d_seeds_kernel, d_followers_kernel, d_points_view, item);
     });
-  })
-  .wait();
+  });
+  (*queue_).wait();
 }
--- src/sycl/plugin-CLUEClusterizer/CLUESYCLClusterizer.cc
+++ src/sycl/plugin-CLUEClusterizer/CLUESYCLClusterizer.cc
@@ -1,13 +1,15 @@
 #include <CL/sycl.hpp>
-#include "SYCLCore/Product.h"
+
 #include "Framework/EventSetup.h"
 #include "Framework/Event.h"
 #include "Framework/PluginFactory.h"
 #include "Framework/EDProducer.h"
-#include "SYCLCore/ScopedContext.h"
 
+#include "SYCLCore/ScopedContext.h"
+#include "SYCLCore/Product.h"
+
+#include "DataFormats/PointsCloud.h"
 #include "SYCLDataFormats/PointsCloudSYCL.h"
-#include "SYCLDataFormats/ClusteredPointsCloudSYCL.h"
 #include "CLUEAlgoSYCL.h"
 
 class CLUESYCLClusterizer : public edm::EDProducer {
@@ -18,28 +20,26 @@ public:
 private:
   void produce(edm::Event& iEvent, const edm::EventSetup& iSetup) override;
 
-  edm::EDGetTokenT<cms::sycltools::Product<PointsCloudSYCL>> tokenPointsCloudSYCL_;
-  edm::EDPutTokenT<cms::sycltools::Product<ClusteredPointsCloudSYCL>> tokenClusters_;
+  edm::EDGetTokenT<cms::sycltools::Product<PointsCloud>> tokenPointsCloudSYCL_;
+  edm::EDPutTokenT<cms::sycltools::Product<PointsCloudSYCL>> tokenClusters_;
 };
 
 CLUESYCLClusterizer::CLUESYCLClusterizer(edm::ProductRegistry& reg)
-    : tokenPointsCloudSYCL_{reg.consumes<cms::sycltools::Product<PointsCloudSYCL>>()},
-      tokenClusters_{reg.produces<cms::sycltools::Product<ClusteredPointsCloudSYCL>>()} {}
+    : tokenPointsCloudSYCL_{reg.consumes<cms::sycltools::Product<PointsCloud>>()},
+      tokenClusters_{reg.produces<cms::sycltools::Product<PointsCloudSYCL>>()} {}
 
 void CLUESYCLClusterizer::produce(edm::Event& event, const edm::EventSetup& eventSetup) {
   auto const& pcProduct = event.get(tokenPointsCloudSYCL_);
   cms::sycltools::ScopedContextProduce ctx(pcProduct);
-  PointsCloudSYCL const& pc = ctx.get(pcProduct);
+  PointsCloud const& pc = ctx.get(pcProduct);
   auto stream = ctx.stream();
   float dc = 20;
   float rhoc = 25;
   float outlierDeltaFactor = 2;
-  CLUEAlgoSYCL clueAlgo(dc, rhoc, outlierDeltaFactor, stream);
+  CLUEAlgoSYCL clueAlgo(dc, rhoc, outlierDeltaFactor, stream, pc.n);
   clueAlgo.makeClusters(pc);
 
-  ClusteredPointsCloudSYCL cpc(pc);
-
-  ctx.emplace(event, tokenClusters_, std::move(cpc));
+  ctx.emplace(event, tokenClusters_, std::move(clueAlgo.d_points));
 }
 
 DEFINE_FWK_MODULE(CLUESYCLClusterizer);
