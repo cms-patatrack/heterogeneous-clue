bb098a8db25a123ab36c2bf2685e66e955ade5de
Juan Jose Olivera Loyola <jolivera@olice-05.cern.ch>
2022-08-12 15:53:29 +0200

    ToyDetector automatic validation with 100k points file coded and passing

--- src/sycl/DataFormats/PointsCloud.h
+++ src/sycl/DataFormats/PointsCloud.h
@@ -4,6 +4,12 @@
 #include <vector>
 
 struct PointsCloud {
+
+  PointsCloud() = default;
+  PointsCloud(int n)
+    : x(n), y(n), layer(n), weight(n),
+      rho(n), delta(n), nearestHigher(n), isSeed(n), clusterIndex(n) {}
+
   std::vector<float> x;
   std::vector<float> y;
   std::vector<int> layer;
@@ -12,9 +18,9 @@ struct PointsCloud {
   std::vector<float> rho;
   std::vector<float> delta;
   std::vector<int> nearestHigher;
-  std::vector<int> clusterIndex;
   std::vector<std::vector<int>> followers;
   std::vector<int> isSeed;
+  std::vector<int> clusterIndex;
   // why use int instead of bool?
   // https://en.cppreference.com/w/cpp/container/vector_bool
   // std::vector<bool> behaves similarly to std::vector, but in order to be space efficient, it:
--- src/sycl/plugin-CLUEValidator/CLUEValidator.cc
+++ src/sycl/plugin-CLUEValidator/CLUEValidator.cc
@@ -1,20 +1,24 @@
 #include <iostream>
 #include <fstream>
-
+#include <type_traits>
+#include <unordered_map>
 #include "DataFormats/FEDRawDataCollection.h"
 #include "Framework/EDProducer.h"
 #include "Framework/Event.h"
 #include "Framework/EventSetup.h"
 #include "Framework/PluginFactory.h"
-
 #include "SYCLCore/Product.h"
 #include "SYCLCore/ScopedContext.h"
-// #include "SYCLCore/device_unique_ptr.h"
-
-// #include "DataFormats/PointsCloud.h"
+#include "DataFormats/PointsCloud.h"
 #include "SYCLDataFormats/PointsCloudSYCL.h"
 #include "CLUEValidatorTypes.h"
-// #include "ValidatorData.h"
+
+std::vector<float> CLAMPED(std::vector<float> in, float upperLimit) {
+  std::vector<float> out(in);
+  for (size_t i = 0; i < out.size(); i++)
+    if (out[i] > upperLimit) out[i] = upperLimit;
+  return out;
+}
 
 class CLUEValidator : public edm::EDProducer {
 public:
@@ -23,7 +27,11 @@ public:
 private:
   void produce(edm::Event& event, edm::EventSetup const& eventSetup) override;
   template <class T>
-  bool arraysAreEqual(T* devicePtr, std::vector<T> trueDataArr, int n, sycl::queue stream);
+  bool arraysAreEqual(std::vector<T>, std::vector<T> trueDataArr);
+  bool arraysClustersEqual(const PointsCloud& devicePC, const PointsCloud& truePC);
+  void transferToHost(const PointsCloudSYCL& pcDevice, PointsCloud& pc, sycl::queue stream);
+  void saveDeviceToOutputFile(const PointsCloud& pc, std::string filePath);
+  void validateOutput(const PointsCloud& pc, std::string trueOutFilePath);
   edm::EDGetTokenT<cms::sycltools::Product<PointsCloudSYCL>> tokenPC_;
 };
 
@@ -31,20 +39,51 @@ CLUEValidator::CLUEValidator(edm::ProductRegistry& reg)
   : tokenPC_(reg.consumes<cms::sycltools::Product<PointsCloudSYCL>>()) {}
 
 template <class T>
-bool CLUEValidator::arraysAreEqual(T* devicePtr, std::vector<T> trueDataArr, int n, sycl::queue stream) {
+bool CLUEValidator::arraysAreEqual(std::vector<T> devicePtr, std::vector<T> trueDataArr) {
   bool sameValue = true;
+  for (size_t i = 0; i < devicePtr.size(); i++) {
+    if (std::is_same<T, int>::value) {
+      sameValue = devicePtr[i] == trueDataArr[i];
+    }
+    else {
+      const float TOLERANCE = 0.001;
+      sameValue = std::abs(devicePtr[i] - trueDataArr[i]) <= TOLERANCE;
+    }
 
-  T* host = new T[n];
-  stream.memcpy(host, devicePtr, sizeof(T) * n).wait();
-
-  for (int i = 0; i < n; i++) {
-    if (host[i] != trueDataArr[i]) {
-      sameValue = false;
+    if (!sameValue) {
+      std::cout << "failed comparison for i=" << i << ", " << devicePtr[i] << " /= " << trueDataArr[i] << std::endl;
       break;
     }
   }
+  return sameValue;
+}
 
-  delete[] host;
+bool CLUEValidator::arraysClustersEqual(const PointsCloud& devicePC, const PointsCloud& truePC) {
+
+  std::unordered_map<int, int> clusterIdMap;
+
+  int n = (int)devicePC.x.size();
+
+  for (int i = 0; i < n; i++) {
+    if (devicePC.isSeed[i]) {
+      clusterIdMap[devicePC.clusterIndex[i]] = truePC.clusterIndex[i];
+    }
+  }
+
+  bool sameValue = true;
+  for (int i = 0; i < n; i++) {
+    int originalClusterId = devicePC.clusterIndex[i];
+    int mappedClusterId = clusterIdMap[originalClusterId];
+    if (originalClusterId == -1)
+      mappedClusterId = -1;
+
+    sameValue = (mappedClusterId == truePC.clusterIndex[i]);
+
+    if (!sameValue) {
+      std::cout << "failed comparison for i=" << i << ", original=" << originalClusterId << ", mapped= " << mappedClusterId << " /= " << truePC.clusterIndex[i] << std::endl;
+      break;
+    }
+  }
 
   return sameValue;
 }
@@ -59,82 +98,100 @@ void CLUEValidator::produce(edm::Event& event, edm::EventSetup const& eventSetup
 
   auto stream = ctx.stream();
 
+  PointsCloud pc(pcDevice.n);
+  transferToHost(pcDevice, pc, stream);
   std::cout << "Num of points: " << pcDevice.n << std::endl;
 
   std::cout << "Saving into " << outDataDir->path_ / "clue_output.csv" << std::endl;
-  std::ofstream clueOut(outDataDir->path_ / "clue_output.csv");
+  saveDeviceToOutputFile(pc, outDataDir->path_ / "clue_output.csv");
+  std::cout << "Results were saved!" << std::endl;
 
-  clueOut << "index,x,y,layer,weight,rho,delta,nh,isSeed,clusterId\n";
+  std::cout << "Validating CLUE output..." << '\n';
+  std::cout << "Validating from " << outDataDir->path_ / "toyDetector_1000.csv" << std::endl;
+  validateOutput(pc, outDataDir->path_ / "toyDetector_1000.csv");
+  std::cout << "CLUE output is correct!" << '\n';
+}
 
-  auto x = new float[pcDevice.n];
-  auto y = new float[pcDevice.n];
-  auto layer = new int[pcDevice.n];
-  auto weight = new float[pcDevice.n];
-  auto rho = new float[pcDevice.n];
-  auto delta = new float[pcDevice.n];
-  auto nearestHigher = new int[pcDevice.n];
-  auto isSeed = new int[pcDevice.n];
-  auto clusterIndex = new int[pcDevice.n];
-  stream.memcpy(x, pcDevice.x.get(), pcDevice.n*sizeof(float));
-  stream.memcpy(y, pcDevice.y.get(), pcDevice.n*sizeof(float));
-  stream.memcpy(layer, pcDevice.layer.get(), pcDevice.n*sizeof(int));
-  stream.memcpy(weight, pcDevice.weight.get(), pcDevice.n*sizeof(float));
-  stream.memcpy(rho, pcDevice.rho.get(), pcDevice.n*sizeof(float));
-  stream.memcpy(delta, pcDevice.delta.get(), pcDevice.n*sizeof(float));
-  stream.memcpy(nearestHigher, pcDevice.nearestHigher.get(), pcDevice.n*sizeof(int));
-  stream.memcpy(isSeed, pcDevice.isSeed.get(), pcDevice.n*sizeof(int));
-  stream.memcpy(clusterIndex, pcDevice.clusterIndex.get(), pcDevice.n*sizeof(int))
+void CLUEValidator::transferToHost(const PointsCloudSYCL& pcDevice, PointsCloud& pc, sycl::queue stream) {
+  pc.n = pcDevice.n;
+  stream.memcpy(pc.x.data(), pcDevice.x.get(), pcDevice.n*sizeof(float));
+  stream.memcpy(pc.y.data(), pcDevice.y.get(), pcDevice.n*sizeof(float));
+  stream.memcpy(pc.layer.data(), pcDevice.layer.get(), pcDevice.n*sizeof(int));
+  stream.memcpy(pc.weight.data(), pcDevice.weight.get(), pcDevice.n*sizeof(float));
+  stream.memcpy(pc.rho.data(), pcDevice.rho.get(), pcDevice.n*sizeof(float));
+  stream.memcpy(pc.delta.data(), pcDevice.delta.get(), pcDevice.n*sizeof(float));
+  stream.memcpy(pc.nearestHigher.data(), pcDevice.nearestHigher.get(), pcDevice.n*sizeof(int));
+  stream.memcpy(pc.isSeed.data(), pcDevice.isSeed.get(), pcDevice.n*sizeof(int));
+  stream.memcpy(pc.clusterIndex.data(), pcDevice.clusterIndex.get(), pcDevice.n*sizeof(int))
     .wait();
+}
 
-
-  for (int i = 0; i < pcDevice.n; i++) {
+void CLUEValidator::saveDeviceToOutputFile(const PointsCloud& pc, std::string filePath) {
+  std::ofstream clueOut(filePath);
+  clueOut << "index,x,y,layer,weight,rho,delta,nh,isSeed,clusterId\n";
+  for (int i = 0; i < pc.n; i++) {
     clueOut << i << ","
-              << x[i] << "," << y[i] << ","
-              << layer[i] << "," << weight[i] << ","
-              << rho[i] << "," << (delta[i] > 999 ? 999 : delta[i]) << ","
-              << nearestHigher[i] << "," << isSeed[i] << ","
-              << clusterIndex[i] << "\n";
+              << pc.x[i] << "," << pc.y[i] << ","
+              << pc.layer[i] << "," << pc.weight[i] << ","
+              << pc.rho[i] << "," << (pc.delta[i] > 999 ? 999 : pc.delta[i]) << ","
+              << pc.nearestHigher[i] << "," << pc.isSeed[i] << ","
+              << pc.clusterIndex[i] << "\n";
   }
 
   clueOut.close();
-
-  delete[] x;
-  delete[] y;
-  delete[] layer;
-  delete[] weight;
-  delete[] rho;
-  delete[] delta;
-  delete[] nearestHigher;
-  delete[] isSeed;
-  delete[] clusterIndex;
-
-//   std::cout << "Checking data on device" << '\n';
-
-//   assert(pcDevice.n == pcTrueData->n);
-//   std::cout << "Number of points -> Ok" << '\n';
-//   assert(arraysAreEqual(pcDevice.x.get(), pcTrueData->x, pcDevice.n, stream));
-//   std::cout << "x -> Ok" << '\n';
-//   assert(arraysAreEqual(pcDevice.y.get(), pcTrueData->y, pcDevice.n, ctx.stream()));
-//   std::cout << "y -> Ok" << '\n';
-//   assert(arraysAreEqual(pcDevice.layer.get(), pcTrueData->layer, pcDevice.n, ctx.stream()));
-//   std::cout << "layer -> Ok" << '\n';
-//   assert(arraysAreEqual(pcDevice.weight.get(), pcTrueData->weight, pcDevice.n, ctx.stream()));
-//   std::cout << "weight -> Ok" << '\n';
-
-//   std::vector<float> fZeros(pcDevice.n, 0);
-//   std::vector<int> iZeros(pcDevice.n, 0);
-
-//   assert(arraysAreEqual(pcDevice.rho.get(), fZeros, pcDevice.n, ctx.stream()));
-//   std::cout << "rho correctly initialised to 0" << '\n';
-//   assert(arraysAreEqual(pcDevice.delta.get(), fZeros, pcDevice.n, ctx.stream()));
-//   std::cout << "delta correctly initialised to 0" << '\n';
-//   assert(arraysAreEqual(pcDevice.nearestHigher.get(), iZeros, pcDevice.n, ctx.stream()));
-//   std::cout << "nearestHigher correctly initialised to 0" << '\n';
-//   assert(arraysAreEqual(pcDevice.clusterIndex.get(), iZeros, pcDevice.n, ctx.stream()));
-//   std::cout << "clusterIndex correctly initialised to 0" << '\n';
-//   assert(arraysAreEqual(pcDevice.isSeed.get(), iZeros, pcDevice.n, ctx.stream()));
-//   std::cout << "isSeed correctly initialised to 0" << '\n';
-  
-  std::cout << "Results were saved!" << std::endl;
 }
+
+void CLUEValidator::validateOutput(const PointsCloud& pc, std::string trueOutFilePath) {
+  PointsCloud truePC;
+  std::ifstream iTrueDataFile(trueOutFilePath);
+  std::string value = "";
+  // Get Header Line
+  getline(iTrueDataFile, value);
+  std::cout << "Readed header: " << value << std::endl;
+  // Iterate through each line and split the content using delimeter
+  int n = 1;
+  while (getline(iTrueDataFile, value, ',')) {
+    int id = std::stoi(value);
+    try {
+      getline(iTrueDataFile, value, ',');
+      truePC.x.push_back(std::stof(value));
+      getline(iTrueDataFile, value, ',');
+      truePC.y.push_back(std::stof(value));
+      getline(iTrueDataFile, value, ',');
+      truePC.layer.push_back(std::stoi(value));
+      getline(iTrueDataFile, value, ',');
+      truePC.weight.push_back(std::stof(value));
+      getline(iTrueDataFile, value, ',');
+      truePC.rho.push_back(std::stof(value));
+      getline(iTrueDataFile, value, ',');
+      truePC.delta.push_back(std::stof(value));
+      getline(iTrueDataFile, value, ',');
+      truePC.nearestHigher.push_back(std::stoi(value));
+      getline(iTrueDataFile, value, ',');
+      truePC.isSeed.push_back(std::stoi(value));
+      getline(iTrueDataFile, value);
+      truePC.clusterIndex.push_back(std::stoi(value));
+    } catch(std::exception& e) {
+      std::cout << e.what() << std::endl;
+      std::cout << "Bad Input: '" << value << "' in line " << n << std::endl;
+      break;
+    }
+    n++;
+  }
+  std::cout << "Readed true points!\n";
+  iTrueDataFile.close();
+
+  std::cout << "0: " << truePC.x[0] << "," << truePC.y[0] << ","
+                    << truePC.layer[0] << "," << truePC.weight[0] << ","
+                    << truePC.rho[0] << "," << truePC.delta[0] << ","
+                    << truePC.nearestHigher[0] << "," << truePC.isSeed[0] << ","
+                    << truePC.clusterIndex[0] << std::endl;
+
+  assert(arraysAreEqual(pc.rho, truePC.rho));
+  assert(arraysAreEqual(CLAMPED(pc.delta, 999), truePC.delta));
+  assert(arraysAreEqual(pc.nearestHigher, truePC.nearestHigher));
+  assert(arraysAreEqual(pc.isSeed, truePC.isSeed));
+  assert(arraysClustersEqual(pc, truePC));
+}
+
 DEFINE_FWK_MODULE(CLUEValidator);
