43db530ac72ad0d278ef8cf1363543a91fe6ffd8
Parsifal-2045 <ferragina.luca@gmail.com>
2022-08-22 14:55:35 +0200

    [sycl] Clusterizer doesn't need loader anymore

--- src/sycl/plugin-CLUEClusterizer/CLUESYCLClusterizer.cc
+++ src/sycl/plugin-CLUEClusterizer/CLUESYCLClusterizer.cc
@@ -21,24 +21,23 @@ public:
 private:
   void produce(edm::Event& iEvent, const edm::EventSetup& iSetup) override;
 
-  edm::EDGetTokenT<cms::sycltools::Product<PointsCloud>> tokenPointsCloudSYCL_;
-  edm::EDPutTokenT<cms::sycltools::Product<PointsCloudSYCL>> tokenClusters_;
+  edm::EDGetTokenT<PointsCloud> pointsCloudToken_;
+  edm::EDPutTokenT<cms::sycltools::Product<PointsCloudSYCL>> clusterToken_;
 };
 
 CLUESYCLClusterizer::CLUESYCLClusterizer(edm::ProductRegistry& reg)
-    : tokenPointsCloudSYCL_{reg.consumes<cms::sycltools::Product<PointsCloud>>()},
-      tokenClusters_{reg.produces<cms::sycltools::Product<PointsCloudSYCL>>()} {}
+    : pointsCloudToken_{reg.consumes<PointsCloud>()},
+      clusterToken_{reg.produces<cms::sycltools::Product<PointsCloudSYCL>>()} {}
 
 void CLUESYCLClusterizer::produce(edm::Event& event, const edm::EventSetup& eventSetup) {
-  auto const& pcProduct = event.get(tokenPointsCloudSYCL_);
-  cms::sycltools::ScopedContextProduce ctx(pcProduct);
-  PointsCloud const& pc = ctx.get(pcProduct);
+  auto const& pc = event.get(pointsCloudToken_);
+  cms::sycltools::ScopedContextProduce ctx(event.streamID());
   Parameters const& par = eventSetup.get<Parameters>();
   auto stream = ctx.stream();
   CLUEAlgoSYCL clueAlgo(par.dc, par.rhoc, par.outlierDeltaFactor, stream, pc.n);
   clueAlgo.makeClusters(pc);
 
-  ctx.emplace(event, tokenClusters_, std::move(clueAlgo.d_points));
+  ctx.emplace(event, clusterToken_, std::move(clueAlgo.d_points));
 }
 
 DEFINE_FWK_MODULE(CLUESYCLClusterizer);
